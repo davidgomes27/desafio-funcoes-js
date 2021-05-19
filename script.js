79f3db949cab8935cc5116e611fda2a3d807052b
dev-davidgomes <davidsmithcrlh@outlook.com>
2021-05-19 03:14:14 -0300

    subindo desafio de funçoes de logica js

---
+++ script.js
@@ -0,0 +1,28 @@
+function mensagem (oi){
+    console.log("ola")
+}
+mensagem()
+
+
+function nome (nome){
+	console.log('meu nome é ' , nome)
+}
+nome("David")
+
+
+function g (nome , idade , ecletico){
+    console.log("meu nome é" , nome ,"meu minha idade é " , idade , "meu estilo musical é " , ecletico)
+}
+g ("David", 27 , "ecletico")
+
+
+function filmeemusica (nome , myboo){
+	console.log('meu filme favorito é ' , nome , "minha musica favolrita é" , myboo)
+}
+filmeemusica("pantera negra","myboo")
+
+function double(num){
+	return num*3
+}
+double(3)
+console.log("o resultado de 3x3 é =",double(3))
